bb638ac0780c3b0aec82d65743ab458beafa2fff
AmdadulMilon <emdadul318@gmail.com>
2021-01-01 09:09:24 +0600

    multiply function added

--- calcu.py
+++ calcu.py
@@ -3,5 +3,8 @@ print(5*8)
 num_one = int(input())
 num_two = int(input())
 
-num_sum = num_one + num_tow
+num_sum = num_one + num_two
 print(num_sum)
+
+multiply = num_one * num_two
+print(multiply)
